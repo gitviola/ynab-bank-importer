916af8684a6775a04af9bc8429c23f6c9d761f3a
Fischer, Fabian <fabian.fischer@siemens.com>
2019-05-17 14:19:59 +0200

    feat: select different config via command line

--- lib/settings.rb
+++ lib/settings.rb
@@ -5,7 +5,13 @@ class Settings
       if ENV['ENV'] == 'test'
         YAML.load_file(File.join('.', 'spec/config.test.yml'))
       else
-        YAML.load_file(File.join('.', 'config.yml'))
+        # check if the first command line parameter contains a value and use that for the config file
+        first_arg, *the_rest = ARGV
+        if !first_arg.to_s.strip.empty?
+          YAML.load_file(File.join('.', first_arg.to_s.strip))
+        else
+          YAML.load_file(File.join('.', 'config.yml'))
+        end
       end
     end
   end
